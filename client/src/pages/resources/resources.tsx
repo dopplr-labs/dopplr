cf20c29da2a2550a8fd3cc07d0413e9bd2419576
hnagrath09 <him.nagrath@gmail.com>
2020-11-07 18:48:17 +0530

    Added side bar to navigate through established connection in Resources page

--- client/src/pages/resources/resources.tsx
+++ client/src/pages/resources/resources.tsx
@@ -1,61 +1,24 @@
 import React from 'react'
-import Banner from './components/banner'
-import ResourceCard from './components/resource-card'
-
-const cardList = [
-  {
-    id: 'postgres',
-    title: 'Postgres',
-    description:
-      'Connect with PostgreSQL database to run SQL queries in Dopplr',
-    imagePath: require('../../images/resources/postgres-logo.png'),
-    commingSoon: false,
-  },
-  {
-    id: 'vertica',
-    title: 'Vertica',
-    description: 'Connect with Vertica database to run SQL queries in Dopplr',
-    imagePath: require('../../images/resources/vertica-logo.png'),
-    comingSoon: true,
-  },
-  {
-    id: 'hive',
-    title: 'Hive',
-    description: 'Connect with Hive database to run SQL queries in Dopplr',
-    imagePath: require('../../images/resources/hive-logo.png'),
-    comingSoon: true,
-  },
-  {
-    id: 'redshift',
-    title: 'Redshift',
-    description: 'Connect with Redshift database to run SQL queries in Dopplr',
-    imagePath: require('../../images/resources/redshift-logo.png'),
-    comingSoon: true,
-  },
-  {
-    id: 'mongodb',
-    title: 'MongoDB',
-    description: 'Connect with MongoDB database to run queries in Dopplr',
-    imagePath: require('../../images/resources/mongodb-logo.png'),
-    comingSoon: true,
-  },
-]
+import { DatabaseOutlined, PlusOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+import { Link } from 'react-router-dom'
+import Connectors from './components/connectors-list'
 
 export default function Resources() {
   return (
-    <div>
-      <Banner />
-      <div className="grid gap-4 p-4 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        {cardList.map((card) => (
-          <ResourceCard
-            key={card.id}
-            title={card.title}
-            description={card.description}
-            imagePath={card.imagePath}
-            comingSoon={card.comingSoon ?? false}
-          />
-        ))}
+    <div className="flex flex-1 h-full">
+      <div className="flex flex-col w-64 h-full p-6 space-y-3 border-r">
+        <div className="flex items-center space-x-2">
+          <DatabaseOutlined className="text-lg" />
+          <span className="font-semibold">Connections</span>
+        </div>
+        <Link to="/resources">
+          <Button type="primary" icon={<PlusOutlined />} className="w-full">
+            Create New
+          </Button>
+        </Link>
       </div>
+      <Connectors />
     </div>
   )
 }
