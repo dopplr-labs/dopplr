7bde0d8f6285f211a50595e944689ae2bfea35d5
Abinash Panda <abinash@prodios.com>
2020-11-15 10:41:16 +0530

    style(navbar): reduce navbar logo, font-size and navbar height

--- client/src/components/nav-bar/nav-bar.tsx
+++ client/src/components/nav-bar/nav-bar.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className="relative z-10 shadow h-14">
+    <div className="relative z-10 h-12 shadow">
       <div className="flex items-center h-full px-4 mx-auto">
         <img
           src={require('images/logo-transparent.svg')}
           alt="Dopplr"
-          className="w-8 h-8 mr-2"
+          className="w-6 h-6 mr-2"
         />
-        <div className="text-lg font-semibold text-gray-700">Dopplr</div>
+        <div className="text-base font-semibold text-gray-700">Dopplr</div>
         <div className="flex-1" />
       </div>
     </div>
