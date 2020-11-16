8b05755e8ad06947686cba6da7e52964ee7c6c94
hnagrath09 <him.nagrath@gmail.com>
2020-11-16 20:37:27 +0530

    Moved fetchSchema query in queries-and-mutation

--- client/src/pages/queries/queries-and-mutations.ts
+++ client/src/pages/queries/queries-and-mutations.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios'
 import { QueryResult } from 'types/query'
+import { SchemaResult } from 'types/schema'
 
 const client = Axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL })
 
@@ -17,5 +18,13 @@ export async function runQuery({
       query,
     },
   )
+  return data.data
+}
+
+export async function fetchSchema(id: number) {
+  const { data } = await client.post<{
+    success: boolean
+    data: SchemaResult[]
+  }>(`/resources/schema/${id}`)
   return data.data
 }
