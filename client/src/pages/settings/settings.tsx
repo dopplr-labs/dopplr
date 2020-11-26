b4f952aa3c6d86c3f0625c0206fc5e43b9e678c3
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-11-26 21:25:45 +0530

    [AakashMallik] Added responsive tags

--- client/src/pages/settings/settings.tsx
+++ client/src/pages/settings/settings.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Input } from 'antd'
-import { AudioOutlined } from '@ant-design/icons'
 
 const { Search } = Input
 
@@ -9,7 +8,7 @@ const onSearch = (value: string): void => console.log(value)
 export default function Settings() {
   return (
     <div className="flex flex-1 h-full justify-center bg-gray-200">
-      <div className="flex flex-col w-1/2 min-w-min-content my-4 shadow-lg">
+      <div className="flex flex-col xl:w-1/2 w-96 my-4 shadow-lg">
         <div className="mx-2 mt-2">
           <Search
             placeholder="input search text"
@@ -18,7 +17,7 @@ export default function Settings() {
           />
         </div>
         <div className="flex flex-1 mx-2 mt-2 mb-2">
-          <div className="w-56 bg-gray-200">Heading</div>
+          <div className="w-56 hidden xl:block bg-gray-200">Heading</div>
           <div className="flex-1 overflow-y-scroll bg-gray-300">Actual settings</div>
         </div>
       </div>
