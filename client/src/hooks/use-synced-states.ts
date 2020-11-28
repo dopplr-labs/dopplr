9199555fe8aefc0a549062a4b4616764a170f295
Abinash Panda <abinash@prodios.com>
2020-11-28 19:48:31 +0530

    feat(query-editor): create component to update query name

---
+++ client/src/hooks/use-synced-states.ts
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+import { isEqual } from 'lodash-es'
+import { usePrevious } from './use-previous'
+
+export function useSyncedState<T extends any>(
+  prop: T,
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(prop)
+
+  const prevProp = usePrevious(prop)
+
+  useEffect(() => {
+    if (!isEqual(prop, prevProp)) {
+      setState(prop)
+    }
+  }, [prop, prevProp])
+
+  return [state, setState]
+}
