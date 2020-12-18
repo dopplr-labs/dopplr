a11f86b99a77d0e6503b0709031c47672046ee0c
Himanshu Nagrath <him.nagrath@gmail.com>
2020-12-18 10:03:26 +0530

    feat(utils/client): added request interceptor to add authorization header in all api requests

--- client/src/utils/client.ts
+++ client/src/utils/client.ts
@@ -1,7 +1,22 @@
 import Axios from 'axios'
+import { auth } from 'utils/firebase'
 
 const client = Axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
+})
+
+client.interceptors.request.use(async (config) => {
+  const currentUser = auth.currentUser
+  if (currentUser) {
+    const idToken = await currentUser.getIdToken()
+    return {
+      ...config,
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    }
+  }
+  return config
 })
 
 export default client
