00c0f6e1d1fa2d7a72cf92695094536528d8d4ca
Abinash Panda <abinash@prodios.com>
2020-12-18 19:55:40 +0530

    feat(docker): update docker-compose files to use env variables

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if [ ! -d "node_modules" ] || [ ! "$(ls -A node_modules)" ]; then
-  echo "Node modules not installed. Installing..."
-  yarn install
-fi
+echo "Installing node modules..."
+yarn install
 
 echo "Starting your app..."
 
