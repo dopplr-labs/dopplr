1f29a9508e87f916f142f1a9d3e8aff506a53fd7
Himanshu Nagrath <him.nagrath@gmail.com>
2020-12-26 19:35:15 +0530

    refactor(vertical-orientation): created a common presentational component for vertical

---
+++ client/src/pages/queries/components/vertical-orientation.tsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import { Tooltip, Tabs, Result } from 'antd'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import { RectReadOnly } from 'react-use-measure'
+import {
+  BorderHorizontalOutlined,
+  RightOutlined,
+  LeftOutlined,
+} from '@ant-design/icons'
+import Editor from 'components/editor'
+import HorizontalPane from 'components/horizontal-pane'
+import { QueryResult } from 'types/query'
+import ResultsTable from './results-table'
+
+type VerticalOrientationProps = {
+  handleHorizontalSplit: () => void
+  resourceId: number
+  query: string
+  setQuery: (udpatedValue: string) => void
+  editorAction: monaco.editor.IActionDescriptor[]
+  containerBounds: RectReadOnly
+  queryResult: QueryResult | null
+  isRunningQuery: boolean
+  error: any
+}
+
+export default function VerticalOrientation({
+  handleHorizontalSplit,
+  resourceId,
+  query,
+  setQuery,
+  editorAction,
+  containerBounds,
+  queryResult,
+  isRunningQuery,
+  error,
+}: VerticalOrientationProps) {
+  return (
+    <HorizontalPane
+      paneName="editor-horizontal-pane"
+      initialWidth={640}
+      maxConstraint={800}
+      minConstraint={320}
+      buffer={160}
+      render={({ paneWidth, isPaneClose, dragHandle, toggleFullScreen }) => {
+        const headerIcons = (
+          <div className="space-x-4">
+            <Tooltip
+              title="Split Horizontally"
+              placement="left"
+              mouseEnterDelay={1}
+            >
+              <button
+                className="focus:outline-none"
+                onClick={handleHorizontalSplit}
+              >
+                <BorderHorizontalOutlined />
+              </button>
+            </Tooltip>
+            <Tooltip title="Fullscreen" placement="left" mouseEnterDelay={1}>
+              <button className="focus:outline-none" onClick={toggleFullScreen}>
+                {isPaneClose ? <RightOutlined /> : <LeftOutlined />}
+              </button>
+            </Tooltip>
+          </div>
+        )
+
+        return (
+          <>
+            <div
+              className="relative z-10 flex flex-col h-full border-r"
+              style={{ width: paneWidth }}
+            >
+              <Editor
+                resourceId={resourceId}
+                value={query}
+                setValue={setQuery}
+                editorAction={editorAction}
+              />
+              {dragHandle}
+            </div>
+            <div
+              className="h-full"
+              style={{ width: containerBounds.width - paneWidth }}
+            >
+              <Tabs
+                defaultActiveKey="1"
+                className="h-full px-4 py-1 queries-tab"
+                size="small"
+                tabBarExtraContent={headerIcons}
+              >
+                <Tabs.TabPane tab="Table" key="1">
+                  <div className="h-full">
+                    <ResultsTable
+                      data={queryResult}
+                      isLoading={isRunningQuery}
+                      error={error}
+                    />
+                  </div>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Charts" key="2">
+                  <Result
+                    title="Under Construction"
+                    subTitle="Sorry, the page doesn't exist. Come back later"
+                  />
+                </Tabs.TabPane>
+              </Tabs>
+            </div>
+          </>
+        )
+      }}
+    />
+  )
+}
