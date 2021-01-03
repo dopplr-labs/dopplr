05ad66a26ced70a6869e6108abf25ae1a842963b
Abinash Panda <abinash@prodios.com>
2021-01-03 22:04:57 +0530

    feat(esbuild): use esbuild instead of babel to build

---
+++ client/craco.config.js
@@ -0,0 +1,68 @@
+const CracoEsbuildPlugin = require('craco-esbuild')
+const CracoLessPlugin = require('craco-less')
+const MonacoEditorWebpackPlugin = require('monaco-editor-webpack-plugin')
+const defaultTailwindConfig = require('tailwindcss/defaultTheme')
+const tailwindConfig = require('./tailwind.config')
+
+function remToPx(spacing) {
+  const remValue = Number.parseFloat(spacing.replace('rem', ''))
+  return `${remValue * 16}px`
+}
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoEsbuildPlugin,
+      options: {
+        esbuildLoaderOptions: {
+          loader: 'tsx',
+          target: 'es2015',
+        },
+      },
+    },
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              '@font-family': tailwindConfig.theme.extend.fontFamily.sans.join(
+                ', ',
+              ),
+              '@primary-color':
+                tailwindConfig.theme.extend.colors.brand.primary,
+              '@border-radius-base': defaultTailwindConfig.borderRadius.DEFAULT,
+              '@padding-sm': remToPx(defaultTailwindConfig.spacing[4]),
+              '@heading-color':
+                tailwindConfig.theme.extend.colors.content.primary,
+              '@text-color':
+                tailwindConfig.theme.extend.colors.content.secondary,
+              '@text-color-secondary':
+                tailwindConfig.theme.extend.colors.content.tertiary,
+              '@form-item-label-font-size': defaultTailwindConfig.fontSize.xs,
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+  webpack: {
+    alias: {
+      vscode: require.resolve('monaco-languageclient/lib/vscode-compatibility'),
+    },
+    plugins: [
+      new MonacoEditorWebpackPlugin({
+        languages: ['sql', 'pgsql'],
+      }),
+    ],
+  },
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss')(require('./tailwind.config')),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
