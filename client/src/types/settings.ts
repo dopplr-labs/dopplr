9e46bcdfd973a1693e6df5bc0f6a453c7efaee5c
humble_D <aakashs.mallik.cse15@itbhu.ac.in>
2021-01-09 20:51:00 +0530

    Custom Vim/Emacs Keybindings supported, Themes support added (#35)
    Co-authored-by: Abinash Panda <abinash@prodios.com>

--- client/src/types/settings.ts
+++ client/src/types/settings.ts
@@ -2,6 +2,17 @@ export type BaseSettings = {
   [key: string]: number | boolean | string
 }
 
+export enum KeyBinding {
+  NONE = 'none',
+  VIM = 'vim',
+  EMACS = 'emacs',
+}
+
 export type LineNumber = 'on' | 'off' | 'relative' | 'interval'
 export type CursorBlinking = 'blink' | 'smooth' | 'phase' | 'expand' | 'solid'
 export type WordWrap = 'off' | 'on' | 'wordWrapColumn' | 'bounded'
+
+export type MinimapScale = 1 | 2 | 3
+export type MiniMapShowSlider = 'always' | 'mouseover'
+export type MiniMapSide = 'right' | 'left'
+export type MiniMapSize = 'proportional' | 'fill' | 'fit'
