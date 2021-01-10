34e6073b4b4e1ae8d6968fc78135f5d7351dde8f
Himanshu Nagrath <him.nagrath@gmail.com>
2021-01-10 16:21:39 +0530

    refactor(dashboard-data): moved dummy data for dashboard to data folder

---
+++ client/src/pages/dashboards/data/dashboard-data.ts
@@ -0,0 +1,34 @@
+export const dashboardList = [
+  {
+    id: '1',
+    name: 'Queries Executed',
+    description:
+      'Dashboard to monitor total queries executed, failed and saved by Dopplr users',
+    categoryId: '1',
+  },
+  {
+    id: '2',
+    name: 'Resources Created',
+    description:
+      'Dashboard to monitor most used databases by the users and total queries ran on them',
+    categoryId: '1',
+  },
+  {
+    id: '3',
+    name: 'Email Campaigns',
+    description: 'Most effective email campaigns going on right now',
+    categoryId: '2',
+  },
+  {
+    id: '4',
+    name: 'Paid Users',
+    description: 'Demography of all paid customers',
+    categoryId: '3',
+  },
+]
+
+export const categories = [
+  { id: '1', name: 'Product' },
+  { id: '2', name: 'Marketing' },
+  { id: '3', name: 'Users' },
+]
