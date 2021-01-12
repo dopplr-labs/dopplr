79eebbb6df9afcbcc4ffaceb9b7483f3d62a433c
Himanshu Nagrath <him.nagrath@gmail.com>
2021-01-12 22:49:02 +0530

    fix(update-chart-dto): removed usage of partial-types library

--- server/src/charts/dto/update-chart.dto.ts
+++ server/src/charts/dto/update-chart.dto.ts
@@ -1,4 +1,1 @@
-import { PartialType } from '@nestjs/mapped-types'
-import { CreateChartDto } from './create-chart.dto'
-
-export class UpdateChartDto extends PartialType(CreateChartDto) {}
+export class UpdateChartDto {}
