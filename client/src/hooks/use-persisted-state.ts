5cad2d58ae30a8fed3a8da2fdd06827f11f511f9
Abinash Panda <abinash@prodios.com>
2021-01-23 17:17:13 +0530

    feat(hooks): removed ununsed hooks

--- client/src/hooks/use-persisted-state.ts
+++ client/src/hooks/use-persisted-state.ts
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { getDataFromLocalStorage } from 'utils/local-storage'
 
 export function usePersistedSetState<T extends any>(
   key: string,
   initialState: T,
-): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [state, setState] = useState(getDataFromLocalStorage(key, initialState))
+): [T, React.Dispatch<React.SetStateAction<T>>, () => void] {
+  const [state, setState] = useState(() =>
+    getDataFromLocalStorage(key, initialState),
+  )
 
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state))
-  }, [key, state])
+  useEffect(
+    function updateStateOnKeyChange() {
+      setState(getDataFromLocalStorage(key, initialState))
+    },
+    // use JSON.stringify instead of direct initial state to prevent infinite loop
+    // as this hook would always be called because of shallow comparision
+    // eslint-disable-next-line
+    [key, JSON.stringify(initialState)],
+  )
 
-  return [state, setState]
+  useEffect(
+    function persistDataToStorage() {
+      window.localStorage.setItem(key, JSON.stringify(state))
+    },
+    [key, state],
+  )
+
+  const clearCache = useCallback(() => {
+    window.localStorage.removeItem(key)
+  }, [key])
+
+  return [state, setState, clearCache]
 }
 
 export default usePersistedSetState
