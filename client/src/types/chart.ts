75a28b37c8d9a54e3e9880d92787711651a85622
Himanshu Nagrath <him.nagrath@gmail.com>
2021-01-31 19:18:12 +0530

    fix(chart-type): changed ChartType to enum

--- client/src/types/chart.ts
+++ client/src/types/chart.ts
@@ -1,24 +1,25 @@
-export type ChartTypes =
-  | 'line'
-  | 'smoothLine'
-  | 'area'
-  | 'stackedArea'
-  | 'stackedArea100'
-  | 'column'
-  | 'stackedColumn'
-  | 'stackedColumn100'
-  | 'bar'
-  | 'stackedBar'
-  | 'stackedBar100'
-  | 'pie'
-  | 'ring'
-  | 'scatter'
+export enum ChartType {
+  LINE = 'line',
+  SMOOTH_LINE = 'smoothLine',
+  AREA = 'area',
+  STACKED_AREA = 'stackedArea',
+  STACKED_AREA_100 = 'stackedArea100',
+  COLUMN = 'column',
+  STACKED_COLUMN = 'stackedColumn',
+  STACKED_COLUMN_100 = 'stackedColumn100',
+  BAR = 'bar',
+  STACKED_BAR = 'stackedBar',
+  STACKED_BAR_100 = 'stackedBar100',
+  PIE = 'pie',
+  RING = 'ring',
+  SCATTER = 'scatter',
+}
 
 export type Chart = {
   id: number
   name: string
   label: string
   values: string[]
-  type: ChartTypes
+  type: ChartType
   query: number
 }
