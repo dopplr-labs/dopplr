e5d4bd491e7d371fbf3a47791719c9b1605fd1cc
Himanshu Nagrath <him.nagrath@gmail.com>
2021-02-07 15:32:53 +0530

    refactor(page-side-bar): removed unused icon prop from the components

--- client/src/pages/settings-panel/settings-panel.tsx
+++ client/src/pages/settings-panel/settings-panel.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { SettingOutlined } from '@ant-design/icons'
 import { Outlet } from 'react-router-dom'
 import PageLayout from 'components/page-layout'
 import PageSideBar from 'components/page-side-bar'
@@ -10,7 +9,6 @@ export default function SettingsPanel() {
     <PageLayout
       sidebar={
         <PageSideBar
-          icon={<SettingOutlined className="text-lg" />}
           title="Settings"
           description="Update the settings of your Dopplr client"
           items={
