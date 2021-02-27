332b21a6896406ee57381d73b2ba42e0b620fa10
Himanshu Nagrath <him.nagrath@gmail.com>
2021-02-27 15:44:26 +0530

    feat(onboard): added UI for loading and error state

--- client/src/pages/onboard/onboarding.tsx
+++ client/src/pages/onboard/onboarding.tsx
@@ -1,3 +1,5 @@
+import { Result } from 'antd'
+import { range } from 'lodash-es'
 import React, { useState, useMemo } from 'react'
 import { useQuery } from 'react-query'
 import Step from './components/step'
@@ -16,11 +18,30 @@ export default function Onboarding() {
 
   const onboardingContent = useMemo(() => {
     if (isLoading) {
-      return <div />
+      return (
+        <div className="w-full">
+          <div className="max-w-4xl pb-8 mx-auto my-8 space-y-4 shadow-lg ">
+            <div className="px-8 py-6 border-b-2">
+              <div className="w-64 h-8 bg-background-secondary animate-pulse" />
+            </div>
+            <div className="px-8 space-y-4">
+              {range(4).map((val) => (
+                <div
+                  className="flex items-center p-6 space-x-4 border rounded-md"
+                  key={val}
+                >
+                  <div className="w-6 h-6 rounded-full bg-background-secondary animate-pulse" />
+                  <div className="w-64 h-6 bg-background-secondary animate-pulse" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )
     }
 
     if (error) {
-      return <div />
+      return <Result status="warning" subTitle={(error as any).message} />
     }
 
     if (onboardingSteps) {
