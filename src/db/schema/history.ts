9412d0a2407baab0079722260eb355122f81d13b
= <rohitbhatwara1@gmail.com>
2023-09-27 15:35:03 +0530

    feat(history): create schema for history

---
+++ src/db/schema/history.ts
@@ -0,0 +1,26 @@
+import { integer, pgTable, serial, text, time } from 'drizzle-orm/pg-core'
+import { InferSelectModel, relations } from 'drizzle-orm'
+import { users } from './auth'
+import { resources } from './resource'
+
+export const history = pgTable('history', {
+  id: serial('id').primaryKey(),
+  createdAt: time('created_at').defaultNow(),
+  query: text('query').notNull(),
+  name: text('name'),
+  createdBy: text('user_id').references(() => users.id),
+  resourceId: integer('resource_id').references(() => resources.id),
+})
+
+export type History = InferSelectModel<typeof history>
+
+export const historyRelations = relations(history, ({ one }) => ({
+  createdBy: one(users, {
+    fields: [history.createdBy],
+    references: [users.id],
+  }),
+  resourceId: one(resources, {
+    fields: [history.resourceId],
+    references: [resources.id],
+  }),
+}))
