7322360c245617b904ec94a71cb797fd2f61c1c8
= <rohitbhatwara1@gmail.com>
2023-10-27 12:41:00 +0530

    feat(migrations): run migrations for chart type

---
+++ src/db/migrations/0006_fantastic_shinobi_shaw.sql
@@ -0,0 +1,5 @@
+ALTER TYPE "chart_type" ADD VALUE 'AREA_CHART';--> statement-breakpoint
+ALTER TYPE "chart_type" ADD VALUE 'GAUGE_CHART';--> statement-breakpoint
+ALTER TYPE "chart_type" ADD VALUE 'SCATTER_CHART';--> statement-breakpoint
+ALTER TYPE "chart_type" ADD VALUE 'HEAT_MAP';--> statement-breakpoint
+ALTER TYPE "chart_type" ADD VALUE 'STAT_CARD';
