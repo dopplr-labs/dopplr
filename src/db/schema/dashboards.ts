fdbc0a112c259a8c029bcf7a413e2a6db29497b4
= <rohitbhatwara1@gmail.com>
2023-10-30 10:02:18 +0530

    feat(dashboard): create schema for dashboard

---
+++ src/db/schema/dashboards.ts
@@ -0,0 +1,23 @@
+import { json, pgTable, serial, text, time, varchar } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { users } from './auth'
+import { chartsToDashboards } from './charts'
+
+export const dashboards = pgTable('dashboards', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  description: text('description'),
+  icon: varchar('icon'),
+  color: varchar('color'),
+  layout: json('layout'),
+  createdAt: time('created_at').defaultNow(),
+  createdBy: text('user_id').references(() => users.id),
+})
+
+export const dashboardRelations = relations(dashboards, ({ one, many }) => ({
+  createdBy: one(users, {
+    fields: [dashboards.createdBy],
+    references: [users.id],
+  }),
+  chartsToDashboards: many(chartsToDashboards),
+}))
