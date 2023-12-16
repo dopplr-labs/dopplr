7df5cbead8e5e80afcd6abb1dca72fa370eba7f7
Trugamr <trugamr@live.in>
2023-12-16 17:16:17 +0530

    fix(mysql): connecton cache

--- src/lib/mysql/client.ts
+++ src/lib/mysql/client.ts
@@ -1,7 +1,13 @@
 import mysql2 from 'mysql2/promise'
 
 const globalForCache = globalThis as unknown as {
-  mySqlConnectionsCache: Map<string, mysql2.Connection>
+  mySqlConnectionsCache: Map<
+    string,
+    {
+      promise?: Promise<mysql2.Connection>
+      connection?: mysql2.Connection
+    }
+  >
 }
 
 const _connectionCaches = globalForCache.mySqlConnectionsCache ?? new Map<string, mysql2.Connection>()
@@ -16,11 +22,22 @@ if (process.env.NODE_ENV !== 'production') {
  * @returns The cached mysql client
  */
 export async function getMySqlClientForConnectionString(connectionString: string) {
+  // Check if we have cached connection for this connection string
   if (!_connectionCaches.get(connectionString)) {
     // eslint-disable-next-line no-console
     console.warn('creating a new client for connection ', connectionString, _connectionCaches)
 
-    _connectionCaches.set(connectionString, await mysql2.createConnection({ uri: connectionString }))
+    _connectionCaches.set(connectionString, {
+      promise: mysql2.createConnection({ uri: connectionString }),
+    })
   }
-  return _connectionCaches.get(connectionString)!
+
+  // Check if promise is resolved to a connection
+  if (!_connectionCaches.get(connectionString)?.connection) {
+    _connectionCaches.set(connectionString, {
+      connection: await _connectionCaches.get(connectionString)?.promise,
+    })
+  }
+
+  return _connectionCaches.get(connectionString)!.connection!
 }
